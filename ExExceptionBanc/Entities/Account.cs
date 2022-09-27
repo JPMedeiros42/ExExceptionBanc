57654666d072761b2b56768fd15934208cae612d
João Pedro <medeirosjpro@hotmail.com>
2022-09-27 16:51:29 -0300

    Exercise Exception Withdraw Banc

---
+++ ExExceptionBanc/Entities/Account.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Globalization;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using ExExceptionBanc.Entities.Exceptions;
+
+namespace ExExceptionBanc.Entities
+{
+    class Account
+    {
+        public int Number { get; set; }
+        public string Holder { get; set; }
+        public double Balance { get; set; }
+        public double WithdrawLimit { get; set; }
+
+        public Account(int number, string holder, double balance, double withdrawLimit)
+        {
+            Number = number;
+            Holder = holder;
+            Balance = balance;
+            WithdrawLimit = withdrawLimit;
+        }
+
+        public void Deposit(double amount)
+        {
+        }
+        public void Withdraw(double amount)
+        {
+            if (amount > WithdrawLimit)
+            {
+                throw new AccountException("The amount exceeds withdraw limit");
+            }
+            if (amount > Balance)
+            {
+                throw new AccountException("Not enough balance");
+            }
+            Balance -= amount;
+        }
+
+        public override string ToString()
+        {
+            return $"Balance: ${Balance.ToString("F2", CultureInfo.InvariantCulture)}";
+        }
+    }
+}
---
+++ ExExceptionBanc/Program.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Globalization;
+using ExExceptionBanc.Entities;
+using ExExceptionBanc.Entities.Exceptions;
+
+namespace ExExceptionBanc
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            try
+            {
+                Console.WriteLine("Enter account data:");
+                Console.Write("Number: ");
+                int number = int.Parse(Console.ReadLine());
+                Console.Write("Holder: ");
+                string name = Console.ReadLine();
+                Console.Write("Initial balance: ");
+                double balance = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+                Console.Write("Withdraw limit: ");
+                double wlimit = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+
+                Account holder = new Account(number,name,balance,wlimit);
+
+                Console.WriteLine();
+                Console.Write("Enter amount for withdraw: ");
+                double withdraw = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+                holder.Withdraw(withdraw);
+                Console.WriteLine("New balance: " + holder);
+            }
+            catch(AccountException e)
+            {
+                Console.WriteLine("Withdraw error: " + e.Message);
+            }
+            catch (FormatException e)
+            {
+                Console.WriteLine("Error in format: " + e.Message);
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine("Unexpected error: " + e.Message);
+            }
+            
+        }
+    }
+}
